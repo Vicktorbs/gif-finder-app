2bbc61d8f80c481f7bf9f19f4cd0e6aaaf2586da
Victor Bolaños <viktorb.132@gmail.com>
2021-12-31 15:00:28 -0600

    Grid card

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGif = async(category) => {
-    const url  = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=8&api_key=sgatASouL6QA0OloiTwxS5gAVHGXDGET`;
+    const url  = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=sgatASouL6QA0OloiTwxS5gAVHGXDGET`;
     const anwer = await fetch(url);
     const { data } = await anwer.json();
 
