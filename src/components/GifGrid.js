7a96fa4d2f2bd30c83db5291a771ec186d87ad47
Victor Bolaños <viktorb.132@gmail.com>
2021-12-31 17:28:54 -0600

    Cards links

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,6 +7,7 @@ export const GifGrid = ({ category }) => {
     // key: sgatASouL6QA0OloiTwxS5gAVHGXDGET
     // const [images, setImages] = useState([])
     const { data:images, loading } = useFetchGifs(category);
+    console.log(images);
 
     return (
         <>
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types'
 
 export const GifGridItem = ({ title, url }) => {
     return (
-        <div className="card animate__animated animate__fadeIn card-background">
-            <img src={ url } alt={ title } />
-            <p>{ title }</p>
-        </div>
+        <a href={ url } target='_blank' rel='noreferrer' className="card card-background">
+            <div className="animate__animated animate__fadeIn">
+                <img src={ url } alt={ title } />
+                <p>{ title }</p>
+            </div>
+        </a>
     )
 }
 
